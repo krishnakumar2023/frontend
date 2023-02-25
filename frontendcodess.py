15013b8e0393d1ffb57c44c55db41229991400a3
krishnakumar2023 <126132811+krishnakumar2023@users.noreply.github.com>
2023-02-25 09:14:02 +0530

    Add files via upload

---
+++ frontendcodess.py
@@ -0,0 +1,177 @@
+Registration Form
+<!DOCTYPE html>
+<html>
+<head> Student Registration Form
+<title>Form</title>
+</head>
+<body>
+<form>
+<p> </p>
+<label for="fname">Name:</label>
+<input type="text" name="fname" id="fn"><br>
+<p>Select the course:</p>
+<input type="radio" name="course" id="bsc" value="bsc">
+<label for=bsc>B.sc</label>
+<input type="radio" name="course" id="ba" value="ba">
+<label fr="ba">B.A</label>
+<input type="radio" name="course" id="bcom" value="bcom">
+<label for="bcom">B.Com</label>
+<p>Choose the Semester</p>
+<input type="checkbox" name="Semester" id="one" value="one">
+<label for="one">One</label>
+<input type="checkbox" name="Semester" id="two" value="two">
+<label for="two">Two</label><br>
+<p> </p>
+<label for="Nationality">Choose your Nationality:</label>
+<select id="Nationality" name="Nationality">
+<option value="indian">Indian</option>
+<option value="other">Other Nationality</option>
+</select>
+<br>
+<p>
+</p>
+<input type="submit" value="submit">
+</form>
+
+
+
+
+
+
+
+ TIME TABLE FOR SEMESTER 1
+<!DOCTYPE html>
+<html>
+<head>
+<h2>Time Table</h2>
+<title>Time Table</title>
+</head>
+<body>
+<table border="5" cellspacing="0" align="centre">
+<tr>
+<thead align="centre"> Semester 1 Time Table
+</thead>
+</tr>
+<tr >
+<td height="1" align="centre">Day</td>
+<td height="1" align="centre">9-11</td>
+<td height="1" align="centre">11-1</td>
+<td height="1" align="centre">1-3</td></tr>
+<tr>
+<td height="1" align="centre">Monday</td>
+<td height="1" align="centre">CSA</td>
+<td height="1" align="centre">Maths</td>
+<td height="1" align="centre">SEC</td>
+</tr>
+<tr>
+<td height="1" align="centre">Tuesday</td>
+<td height="1" align="centre">CSA</td>
+<td height="1" align="centre">VAC</td>
+<td height="1" align="centre">G.E.</td>
+</tr>
+<tr>
+<td height="1" align="centre">Wednesday</td>
+<td height="1" align="centre">CSA</td>
+<td height="1" align="centre">Maths</td>
+<td height="1" align="centre">SEC</td>
+</tr>
+<tr>
+<td height="1" align="centre">Thrusday</td>
+<td height="1" align="centre">Python</td>
+<td height="1" align="centre">Maths</td>
+<td height="1" align="centre">AECC</td>
+</tr>
+<tr>
+<td height="1" align="centre">Friday</td>
+<td height="1" align="centre">CSA</td>
+<td height="1" align="centre">Python</td>
+<td height="1" align="centre">Java</td>
+</tr>
+</table>
+</body>
+</html>
+
+
+
+ CREATE AN HTML DOCUMENT WITH ORDERED AND
+UNORDERED LIST,INSERTING IMAGES,INTERNAL AND EXTERNAL
+LINKS. <!DOCTYPE html>
+<html>
+<head>
+<h4>The list :</h4>
+<title>OrderedAndUnordered</title>
+</head>
+<body>
+<p>The ordered list</p>
+<ol type="123"><p>The mega cities of India:</p>
+<li>Delhi</li>
+<li>Mumbai</li>
+<li>Chennai</li>
+<li>Kolkata</li>
+</ol>
+<p>The unordered list</p>
+<ul type="square"><p>The States of India:</p>
+<li>Rajasthan</li>
+<li>Maharashtra</li>
+<li>Gujrat</li>
+<li>Tamil Nadu</li>
+</ul>
+<p>This is the image of lights:</p>
+<img src="img_lights.jpg" alt="lights" width="200" height="200">
+<p>External and internal linking:</p>
+<a href="biodata.html">External links</a><br>
+<a href="img_lights.jpg">Internal links</a>
+</body>
+</html>
+
+
+
+
+ CREATE A HTML DOCUMENT WITH FOLLOWING
+FORMATTING: BOLD,ITALICS,UNDERLINE,LINE
+BREAKERS,COLORS,HEADING ETC. <!DOCTYPE html>
+<html>
+<head>
+HTML Program
+<title> SAMPLE</title>
+</head>
+<body>
+<h1>This is h1</h1>
+<h2>This is h2</h2>
+<h3>This is h3</h3>
+<p>This is paragraph</p>
+<p><b>This is line using bold tag</b></p>
+<p><i>This is line in italics</i></p>
+<p><u>This is line using underline tag</u></p>
+<p>This paragraph is written using break line<br>
+the line after break line</p>
+<font>This is font</font>
+</body>
+</html>
+
+
+
+
+ VALIDATION
+<!DOCTYPE html>
+<html>
+<head>
+<title>VALIDATIONFORM</title>
+</head>
+<body>
+<form>
+<label for="fname">Enter your name:</label>
+<input type="text" name="fname" id="fname" required>
+<label for="password">Enter your password:</label>
+<input type="text" name="password" id="password" required>
+<p>Select the course:</p>
+<input type="radio" name="course" id="bsc" value="bsc" required>
+<label for=bsc>B.sc</label>
+<input type="radio" name="course" id="ba" value="ba">
+<label fr="ba">B.A</label>
+<input type="radio" name="course" id="bcom" value="bcom">
+<label for="bcom">B.Com</label>
+<
+</form>
+</body>
+</html>
